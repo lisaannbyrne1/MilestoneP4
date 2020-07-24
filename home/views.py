87390b0fea5da03cd5503dc5490998d846e28eab
Lisa Byrne <lisaannbyrne1@hotmail.com>
2020-07-24 14:35:00 +0000

    fix bug

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-import os
+
 # Create your views here.
+
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html', {"test": tester})
+
+    return render(request, 'home/index.html')
